59cd80bdce037aa777d8c30106c8518ba2a8d2a8
mowtiie <mowtiie.dev@gmail.com>
2026-07-22 13:40:23 +0800

    Added work manager dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
+    implementation(libs.work.runtime)
     testImplementation(libs.junit)
     testImplementation(libs.json)
     androidTestImplementation(libs.espresso.core)
